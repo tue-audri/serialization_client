709503402d401736f97d19e1b8d4d54ab2d72c3c
subramanians1 <152851798+subramanians1@users.noreply.github.com>
2025-09-03 15:31:04 +0200

    Modified node to receive TF info from tf_static topic. Also modified the structure of the config file

--- serialization_client/serialization_client/dynamic_json_serializer.py
+++ serialization_client/serialization_client/dynamic_json_serializer.py
@@ -3,44 +3,93 @@ import os
 from ament_index_python.packages import get_package_share_directory
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import QoSProfile, QoSDurabilityPolicy, QoSReliabilityPolicy
 import yaml
 import json
 import importlib
 from serialization_client import serializers
+from tf2_msgs.msg import TFMessage
 import paho.mqtt.client as mqtt
 
 class DynamicSerializerNode(Node):
     def __init__(self,config_path):
         super().__init__('dynamic_json_serializer_node')
-
-        # URDF path
-        package_share = get_package_share_directory('serialization_client')
-        sensor_calib = os.path.join(package_share, 'config', 'prius_sensor_kit.yaml')
-        
-        # MQTT Setup
-        self.mqtt_client = mqtt.Client()
-        self.mqtt_client.connect("localhost", 1883) # Replace with correct Broker IP and Port
-
+        # Load config file
         with open(config_path, 'r') as file:
             config = yaml.safe_load(file)
 
-        with open(sensor_calib, 'r') as file:
-            calib = yaml.safe_load(file)
+        self.transforms = [] # TF cache
 
-        agent_id = config.get('agent_id', 'default_agent')
-        for topic in config['topics']:
+        qos = QoSProfile(
+            depth=1,  # keep last sample
+            durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
+            reliability=QoSReliabilityPolicy.RELIABLE
+        )
+
+        # subscribe to tf_static
+        self.create_subscription(TFMessage, '/tf_static', self.tf_callback, qos)
+        self.get_logger().info(f"Subscribed to tf_static with type TFMessage")
+        
+
+        mqtt_config = config.get("mqtt", None)
+
+        if mqtt_config:
+            hostname = mqtt_config.get("hostname", None)
+            port = mqtt_config.get("port", None)
+            # MQTT Setup
+            self.mqtt_client = mqtt.Client()
+            self.mqtt_client.connect(hostname, port)
+        else:
+            self.get_logger().info("MQTT config details not found")
+
+        # URDF path
+        # package_share = get_package_share_directory('serialization_client')
+        # sensor_calib = os.path.join(package_share, 'config', 'prius_sensor_kit.yaml')
+
+        # with open(config_path, 'r') as file:
+        #     config = yaml.safe_load(file)
+
+        # with open(sensor_calib, 'r') as file:
+        #     calib = yaml.safe_load(file)
+
+
+        agent_id = config.get("agent",{}).get("details",{}).get('agent_id', 'default_agent')
+        
+
+        for key, topic in config['agent']['static_topics'].items():
+            if isinstance(topic,str):
+                config["agent"]["static_topics"][key] = topic.format(agent_id = agent_id)
+            
+        for topic in config['agent']['ros_topics']:
             topic['mqtt_topic'] = topic['mqtt_topic'].replace('{agent_id}', agent_id)
 
-        self.announcement_config = config.get("static_announcement", None)
-        payload = self.announcement_config.get("payload", None)
-        payload["urdf"] = calib
-        self.announcement_config["payload"] = payload
-
         
+        # Generate static announcement
+        self.announcement_config = {"mqtt_topic": config['agent']['static_topics']['mqtt_topic'],
+                               "payload": {
+                                   "agent_id": config['agent']['details']['agent_id'],
+                                   "make": config['agent']['details']['make'],
+                                   "model": config['agent']['details']['model'],
+                                   "urdf": self.transforms,
+                                   "publications": []
+                               }
+                            }
+        # Add publications to static announcement
+        for topic in config["agent"]["ros_topics"]:
+            mqtt_topic = topic.get("mqtt_topic")
+            if mqtt_topic:
+                self.announcement_config["payload"]["publications"].append(mqtt_topic)
+        
+
+        # self.announcement_config = config.get("static_announcement", None)
+        # payload = self.announcement_config.get("payload", None)
+        # payload["urdf"] = calib
+        # self.announcement_config["payload"] = payload
+
         self.create_timer(5.0,self.publish_announcement)
         
 
-        for topic in config['topics']:
+        for topic in config['agent']['ros_topics']:
             topic_name = topic['name']
             msg_type_str = topic['type']
             serializer_fn_name = topic['serializer']
@@ -59,7 +108,7 @@ class DynamicSerializerNode(Node):
                 def callback(msg):
                     json_str = serializer_fn(msg)
                     self.mqtt_client.publish(mqtt_topic,json_str)
-                    self.get_logger().info(f"[MQTT:{mqtt_topic}] Published JSON")
+                    # self.get_logger().info(f"[MQTT:{mqtt_topic}] Published JSON")
                 return callback
 
             callback = make_callback(topic_name, serializer_fn,mqtt_topic)
@@ -74,6 +123,33 @@ class DynamicSerializerNode(Node):
                 payload = json.dumps(self.announcement_config['payload'])
                 self.mqtt_client.publish(mqtt_topic,payload=payload, retain=True)
                 print(f"Published announcement to {mqtt_topic}")
+                # print(f"Published TF {self.transforms}")
+
+    def tf_callback(self, msg: TFMessage):
+        self.transforms.clear()
+        print("Hello from tf_callback")
+        for transform in msg.transforms:
+            parent = transform.header.frame_id
+            child = transform.child_frame_id
+
+            if(parent == "base_link" and child == "sensor_kit_base_link") or (parent == "sensor_kit_base_link"):
+                tf_dict = {
+                    "parent": parent,
+                    "child": child,
+                    "translation": {
+                        "x": transform.transform.translation.x,
+                        "y": transform.transform.translation.y,
+                        "z": transform.transform.translation.z,
+                    },
+                    "rotation": {
+                        "x": transform.transform.rotation.x,
+                        "y": transform.transform.rotation.y,
+                        "z": transform.transform.rotation.z,
+                        "w": transform.transform.rotation.w,
+                    }
+                }
+                self.transforms.append(tf_dict)
+        self.get_logger().info(f"Cached {len(self.transforms)} static TFs")
 
 def main():
     rclpy.init()
